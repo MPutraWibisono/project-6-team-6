e0c7ac77d162c6c9f395eb5fc0d90404c7c2a762
Abdurrohman Azis <herdimamdra@gmail.com>
2023-11-08 21:36:42 +0700

    movie app v2

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-gradient-to-r from-red-700 to-red-900">
-      <p className="text-center text-white p-8">
+      <p className="p-8 text-center text-white">
         &copy; Copyright 2023, College Student Binar by{" "}
         <span className="font-bold">Team 6 - FEJS-3 </span>| All Rights Reserved
       </p>
--- src/Pages/Id.jsx
+++ src/Pages/Id.jsx
@@ -3,17 +3,17 @@ function Id() {
     <section className="flex justify-center items-center bg-[url('/background.jpg')] bg-cover py-8 min-h-screen w-full contrast-150">
       <div className="max-w-3xl md:max-w-5xl">
         <div className="m-4">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl xl:text-5xl text-white font-bold text-center">
-            Welcome to the Team6 Movie website, where this website can help you
-            find popular films
+          <h1 className="text-2xl font-bold text-center text-white sm:text-3xl md:text-4xl xl:text-5xl">
+            Welcome to the Movie App Website Staging, where this website can
+            help you find popular films
           </h1>
-          <p className="text-white text-xl md:text-2xl text-center font-semibold mt-8 mb-7">
+          <p className="mt-8 text-xl font-semibold text-center text-white md:text-2xl mb-7">
             Login now, to search for films
           </p>
           <div className="text-center">
             <a
               href="/login"
-              className="px-3 py-1 md:px-6 md:py-2 bg-red-600 border-2 border-red-600 hover:bg-red-700 text-white rounded-full font-bold  transition-all duration-300"
+              className="px-3 py-1 font-bold text-white transition-all duration-300 bg-red-600 border-2 border-red-600 rounded-full md:px-6 md:py-2 hover:bg-red-700"
             >
               Login Now!
             </a>
